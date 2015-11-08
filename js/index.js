a1dce8930a03f8a1445c3ff4f2f8c21f8a0cbc7c
zhangshy <zaozh1990@163.com>
2015-11-08 17:02:31 +0800

    显示当月日历

--- js/index.js
+++ js/index.js
@@ -16,6 +16,11 @@ var CalendarHeader = React.createClass({
                 React.createElement(
                     'li',
                     null,
+                    '日'
+                ),
+                React.createElement(
+                    'li',
+                    null,
                     '一'
                 ),
                 React.createElement(
@@ -42,11 +47,6 @@ var CalendarHeader = React.createClass({
                     'li',
                     null,
                     '六'
-                ),
-                React.createElement(
-                    'li',
-                    null,
-                    '日'
                 )
             )
         );
@@ -55,9 +55,7 @@ var CalendarHeader = React.createClass({
 
 var CalendarDay = React.createClass({
     render: function () {
-        var date = this.props.date;
-        var mdate = date.getDate();
-        var day = date.getDay();
+        var mdate = this.props.mdate;
         return React.createElement(
             'div',
             { className: 'calendarDay' },
@@ -69,24 +67,55 @@ var CalendarDay = React.createClass({
 
 var CalendarDays = React.createClass({
     render: function () {
-        var today = new Date();
+        var days = [];
+        var year = this.props.year;
+        var month = this.props.month;
+        var mdate = this.props.mdate;
+        var firstDate = new Date(year, month, 1); // 月第一天
+        var lastDate = new Date(year, month + 1, 0); // 月最后一天
+        console.log(firstDate.getDate(), firstDate.getDay());
+        console.log(lastDate.getDate(), lastDate.getDay());
+        var firstDateDay = firstDate.getDay(); // 第一天周几，0是周日，1是周一
+        var lastDateDay = lastDate.getDay();
+        if (firstDateDay != 0) {
+            var previousMonthLastDate = new Date(year, month, 0);
+            var end = previousMonthLastDate.getDate();
+            for (var i = end - firstDateDay + 1; i <= end; i++) {
+                days.push(React.createElement(CalendarDay, { mdate: i }));
+            }
+        }
+        for (var i = 1; i <= lastDate.getDate(); i++) {
+            days.push(React.createElement(CalendarDay, { mdate: i }));
+            if ((i + firstDateDay) % 7 == 0) {
+                days.push(React.createElement('div', { className: 'clear' }));
+            }
+        }
+        if (lastDateDay != 6) {
+            for (var i = 1; i <= 6 - lastDateDay; i++) {
+                days.push(React.createElement(CalendarDay, { mdate: i }));
+            }
+        }
         return React.createElement(
             'div',
             { className: 'calendarDays' },
-            React.createElement(CalendarDay, { date: today }),
-            React.createElement(CalendarDay, { date: today })
+            days
         );
     }
 });
 
 var Calendar = React.createClass({
     render: function () {
+        var today = new Date();
+        var month = today.getMonth();
+        var year = today.getFullYear();
+        var mdate = today.getDate();
+        console.log(year, month);
         return React.createElement(
             'div',
             { className: 'calendar' },
             React.createElement(CalendarHeader, null),
             React.createElement('div', { className: 'clear' }),
-            React.createElement(CalendarDays, null)
+            React.createElement(CalendarDays, { year: year, month: month, mdate: mdate })
         );
     }
 });
--- jsx/index.jsx
+++ jsx/index.jsx
@@ -10,7 +10,7 @@ var CalendarHeader = React.createClass({
         return (
             <div className="calendarHeader">
                 <ol>
-                    <li>一</li><li>二</li><li>三</li><li>四</li><li>五</li><li>六</li><li>日</li>
+                    <li>日</li><li>一</li><li>二</li><li>三</li><li>四</li><li>五</li><li>六</li>
                 </ol>
             </div>
         );
@@ -19,9 +19,7 @@ var CalendarHeader = React.createClass({
 
 var CalendarDay = React.createClass({
     render: function() {
-        var date = this.props.date;
-        var mdate = date.getDate();
-        var day = date.getDay();
+        var mdate = this.props.mdate;
         return(
             <div className="calendarDay">
                 {mdate}
@@ -33,11 +31,37 @@ var CalendarDay = React.createClass({
 
 var CalendarDays = React.createClass({
     render: function() {
-        var today = new Date();
+        var days = [];
+        var year = this.props.year;
+        var month = this.props.month;
+        var mdate = this.props.mdate;
+        var firstDate = new Date(year, month, 1); // 月第一天
+        var lastDate = new Date(year, month+1, 0); // 月最后一天
+        console.log(firstDate.getDate(),firstDate.getDay());
+        console.log(lastDate.getDate(),lastDate.getDay());
+        var firstDateDay = firstDate.getDay(); // 第一天周几，0是周日，1是周一
+        var lastDateDay = lastDate.getDay();
+        if (firstDateDay!=0) {
+            var previousMonthLastDate = new Date(year, month, 0);
+            var end = previousMonthLastDate.getDate();
+            for(var i=end-firstDateDay+1;i<=end;i++) {
+                days.push(<CalendarDay mdate={i}/>)
+            }
+        }
+        for (var i=1; i<=lastDate.getDate(); i++) {
+            days.push(<CalendarDay mdate={i}/>)
+            if ((i+firstDateDay)%7==0) {
+                days.push(<div className="clear"></div>);
+            }
+        }
+        if (lastDateDay!=6) {
+            for (var i=1;i<=6-lastDateDay;i++) {
+                days.push(<CalendarDay mdate={i}/>)
+            }
+        }
         return(
             <div className="calendarDays">
-                <CalendarDay date={today}/>
-                <CalendarDay date={today}/>
+                {days}
             </div>
         );
     }
@@ -45,11 +69,16 @@ var CalendarDays = React.createClass({
 
 var Calendar = React.createClass({
     render: function () {
+        var today = new Date();
+        var month = today.getMonth();
+        var year = today.getFullYear();
+        var mdate = today.getDate();
+        console.log(year,month);
         return (
             <div className="calendar">
                 <CalendarHeader />
                 <div className="clear"></div>
-                <CalendarDays/>
+                <CalendarDays year={year} month={month} mdate={mdate}/>
             </div>
         );
     }
